bc87c9251ec988f2581cf48bbd75ae5b284f4d7d
7profit <khatri.piyush233@gmail.com>
2021-07-28 11:36:17 +0530

    Login Page

---
+++ Link.js
@@ -0,0 +1,14 @@
+import firebase from 'firebaseapp';
+import 'firebase/auth';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAj12llGLgdzDywGBQkrVMaydyYwQcIs3g",
+    authDomain: "login-page-27c16.firebaseapp.com",
+    projectId: "login-page-27c16",
+    storageBucket: "login-page-27c16.appspot.com",
+    messagingSenderId: "845852520585",
+    appId: "1:845852520585:web:65b5f5f345017216fcd658",
+    measurementId: "G-NDYGK81LFF"
+  };
+
+  firebase.initializeApp(firebaseConfig);
